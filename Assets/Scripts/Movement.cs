46c556389f772a351be0bd3bc81a0679ee1d2bcd
Patrick Ouimet <pat_117@hotmail.com>
2026-09-22 11:01:16 -0400

    Added first InputAction

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,7 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Movement : MonoBehaviour
-{
-    
+public class Movement : MonoBehaviour {
+    [SerializeField] private InputAction thrust;
+
+    private void OnEnable() {
+        thrust.Enable();
+    }
+
+    private void Update() {
+        if (thrust.WasPressedThisFrame()) {
+            Debug.Log("Thrust");
+        }
+    }
 }
