c5939588a312e2a040dc113a14a46cf99c91fac9
Patrick Ouimet <pat_117@hotmail.com>
2026-09-23 11:43:50 -0400

    Added Movement up
    Within FixedUpdate() with Time.FixedDeltaTime

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,24 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour {
     [SerializeField] private InputAction thrust;
+    Rigidbody rb;
+    [SerializeField] float thrustStrength = 10f;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable() {
         thrust.Enable();
     }
 
-    private void Update() {
-        if (thrust.WasPressedThisFrame()) {
-            Debug.Log("Thrust");
+    private void FixedUpdate() {
+        if (thrust.IsPressed()) {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
 }
